55c083bf2aff09b5a39b30f6b2752dc00e15982c
José Cañete <josecannete@gmail.com>
2023-04-09 18:02:57 -0400

    update

---
+++ 1816B.py
@@ -0,0 +1,28 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        greater = 2*n
+        lower = 1
+        l1 = []
+        l2 = []
+        for i in range(n):
+            if i % 2 == 0:
+                l1.append(greater)
+                l2.append(lower)
+            else:
+                l1.append(lower)
+                l2.append(greater)
+            greater -= 1
+            lower += 1
+
+        l2[1] = l2[-1]
+        l2[-1] = 2*n - 1
+
+
+        print(*l1)
+        print(*l2)
+
+
+if __name__ == "__main__":
+    main()
